1da4e9c5593468a57b677b3a726144ee274ef4cf
Jinda Han <hanjinda@users.noreply.github.com>
2016-04-02 02:56:48 +0000

    Added files via upload

--- similar_icon.py
+++ similar_icon.py
@@ -1,1 +1,19 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+# take some sample and and located at each class folder, 
+# then checking the random folder compare with all the icons in classes' folder, 
+# if similar smaller than [value], then move (or copy) the icons to each class's folder
+
+import json
+import itertools
+import os, sys
+# from classify_nn import Classifying
+# from database import Database
+# from shutil import copyfile
+# import pypuzzle
+# import pydeep
+# puzzle = pypuzzle.Puzzle()
+
+
+
 
